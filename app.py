76a8497b8a730a90c6f38e16bffad1a0917f6501
Mohammed <mohammedabdalla38@yahoo.com>
2021-05-25 20:34:24 -0500

    add routes

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+# consumer Api
+@app.route('/topic/<topicname>')
+def get_messsage(topicname):
+    client = get_kafka_client()
+    def events():
+        for i in client.topics[topicname].get_simple_consumer():
+            yield 'data:{o}\n\n'.format(i.value.decode('ascii'))
+    return Response(events(), mimetype="text/event-stream")
 
 
 
